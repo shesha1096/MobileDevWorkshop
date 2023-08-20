53ecd6b7954707ab7da6c840fa2f4076d0d0b2da
Shesha Shankar <shesha1096@gmail.com>
2023-08-20 19:06:27 +1000

    Exploring constraint layouts

---
+++ ConstraintLayout/app/src/main/java/com/shesha/mobiledevworkshop/constraintlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shesha.mobiledevworkshop.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Replace this with the relevant layouts from the layout directory to explore further
+        setContentView(R.layout.vertical_guideline_layout);
+    }
+}
